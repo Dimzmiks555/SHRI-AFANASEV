0aa9e7a6ff95a7dba50c3c25c2c176e5260b79d7
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2021-03-27 08:02:47 +0300

    js

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ const express = require('express');
 const port = 8080;
 const app = express();
 let jsonData = require('./data/data.json');
+let html = `<html>
+<head>
+    <link rel="stylesheet" href="stories.css">
+</head>
+<body class="theme_light">
+    <script type="text/javascript" src="stories.js"></script>
+    <script>
+        const body = document.querySelector('body');
+        body.innerHTML = window.renderTemplate();
+    </script>
+    
+</body>
+</html>`
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + '/build'));
 app.get("/", function(request, response){
     let slide = request.query.slide;
     sendSlide = jsonData[slide - 1];
-    response.render("index", sendSlide);
+    JSON.stringify(sendSlide);
+    response.write(sendSlide);
+    response.write(html);
+    response.end();
 }); 
+
 // Start the server
 const server = app.listen(port, (error) => {
     if (error) return console.log(`Error: ${error}`);
