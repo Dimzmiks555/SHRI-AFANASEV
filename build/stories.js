b2e8d36ceae69928003f3d79fb49383ee677ba8f
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2021-04-01 17:54:17 +0300

    reworking js

--- build/stories.js
+++ build/stories.js
@@ -1,108 +1,162 @@
  // DATA
- window.renderTemplate = function(alias, data) {
-   console.log(data);
-  return `<head>
-            <h1 class="slide_title">${data.title}</h1>
-            <h2 class="slide_subtitle">${data.subtitle}</h2>
-            </head>
-            <div class="users">
-                <div class="user third left">
-                  <div class="user_info people">
-                    <div class="user_img">
-                        <img src="assets/images/1x/${data.users[4].avatar}" alt="">           
-                    </div>
-                    <div class="user_name">
-                        ${data.users[4].name}             
-                    </div>  
-                    <div class="user_commits">
-                        ${data.users[4].valueText}             
-                    </div>    
-                  </div>     
-                  <div class="user_stend">
-                        5         
-                  </div>
-              
-                </div>  
-                <div class="user second left">
-                  <div class="user_info">
-                    <div class="user_img">
-                        <img src="assets/images/1x/${data.users[2].avatar}" alt="">           
-                    </div>
-                    <div class="user_name">
-                        ${data.users[2].name}             
-                    </div>  
-                    <div class="user_commits">
-                        ${data.users[2].valueText}             
-                    </div>    
-                  </div>     
-                  <div class="user_stend">
-                        3          
-                  </div>
-              
-                </div>    
-                <div class="user top">
-                  <div class="user_info">
-                    
-                    <div class="user_img">
-                        <div class="user_emoji">
-                          ${data.emoji}          
-                        </div>
-                        <img src="assets/images/1x/${data.users[0].avatar}" alt="">           
-                    </div>
-                    <div class="user_name">
-                        ${data.users[0].name}             
-                    </div>  
-                    <div class="user_commits">
-                        ${data.users[0].valueText}             
-                    </div>    
-                  </div>     
-                  <div class="user_stend">
-                        1          
-                  </div>
-              
-                </div>     
-                 <div class="user right second">
-                   <div class="user_info">
-                      <div class="user_img">
-                          <img src="assets/images/1x/${data.users[1].avatar}" alt="">           
-                      </div>
-                      <div class="user_name">
-                          ${data.users[1].name}             
-                      </div>  
-                      <div class="user_commits">
-                          ${data.users[1].valueText}             
-                      </div>    
-                    </div>     
-                    <div class="user_stend">
-                          2        
-                    </div>
-                
-                </div>
-                <div class="user right third">
-                   <div class="user_info">
-                      <div class="user_img">
-                          <img src="assets/images/1x/${data.users[3].avatar}" alt="">           
-                      </div>
-                      <div class="user_name">
-                          ${data.users[3].name}             
-                      </div>  
-                      <div class="user_commits">
-                          ${data.users[3].valueText}             
-                      </div>    
-                    </div>     
-                    <div class="user_stend">
-                          4          
-                    </div>
-                
-                </div>            
-            </div>
+window.renderTemplate = function(alias, data) {
+  
+  let name = data.users[4].name.replace(" ", "<br>");
+  //Header
+
+  let header = document.createElement('header');
+
+      // Title
+
+      let title = document.createElement('h1');
+      title.className = 'slide_title';
+      title.innerHTML = data.title;
+      header.append(title);
+
+      // Subtitle 
+
+      let subtitle = document.createElement('h2');
+      subtitle.className = 'slide_subtitle';
+      subtitle.innerHTML = data.subtitle;
+      header.append(subtitle);
+
+  // Content
+  let content = document.createElement('main');
+
+  if (alias == 'leaders') {
+
+    // Users 
+
+    let users = document.createElement('div');
+    users.className = 'users';
+    content.append(users);
+
+      // User Top
+      let userTop = document.createElement('div');
+      userTop.className = 'user top';
+      users.append(userTop);
+
+      // User Info
+      let userInfo = document.createElement('div');
+      userInfo.className = 'user_info';
+      users.append(userTop);
+
+
+
+      //        <div class="user_info">
+               
+      //          <div class="user_img">
+      //              <div class="user_emoji">
+      //                ${data.emoji}          
+      //              </div>
+      //              <img src="assets/images/1x/${data.users[0].avatar}" alt="">           
+      //          </div>
+      //          <div class="user_name">
+      //              ${data.users[0].name}             
+      //          </div>  
+      //          <div class="user_commits">
+      //              ${data.users[0].valueText}             
+      //          </div>    
+      //        </div>     
+      //        <div class="user_stend">
+      //              1          
+      //        </div>
+         
+    
+
+  }
+
+  
+
+  // Return HTML
+  return (header.outerHTML + content.outerHTML);
+   
+
+
+//    if (alias == 'leaders') {
+//      let content = `<div class="users">
+//      <div class="user third left">
+//        <div class="user_info people">
+//          <div class="user_img">
+//              <img src="assets/images/1x/${data.users[4].avatar}" alt="">           
+//          </div>
+//          <div class="user_name">
+//              ${name}             
+//          </div>  
+//          <div class="user_commits">
+//              ${data.users[4].valueText}             
+//          </div>    
+//        </div>     
+//        <div class="user_stend">
+//              5         
+//        </div>
+   
+//      </div>  
+//      <div class="user second left">
+//        <div class="user_info">
+//          <div class="user_img">
+//              <img src="assets/images/1x/${data.users[2].avatar}" alt="">           
+//          </div>
+//          <div class="user_name">
+//              ${data.users[2].name}             
+//          </div>  
+//          <div class="user_commits">
+//              ${data.users[2].valueText}             
+//          </div>    
+//        </div>     
+//        <div class="user_stend">
+//              3          
+//        </div>
+   
+//      </div>    
+//      
+//       <div class="user right second">
+//         <div class="user_info">
+//            <div class="user_img">
+//                <img src="assets/images/1x/${data.users[1].avatar}" alt="">           
+//            </div>
+//            <div class="user_name">
+//                ${data.users[1].name}             
+//            </div>  
+//            <div class="user_commits">
+//                ${data.users[1].valueText}             
+//            </div>    
+//          </div>     
+//          <div class="user_stend">
+//                2        
+//          </div>
+     
+//      </div>
+//      <div class="user right third">
+//         <div class="user_info">
+//            <div class="user_img">
+//                <img src="assets/images/1x/${data.users[3].avatar}" alt="">           
+//            </div>
+//            <div class="user_name">
+//                ${data.users[3].name}             
+//            </div>  
+//            <div class="user_commits">
+//                ${data.users[3].valueText}             
+//            </div>    
+//          </div>     
+//          <div class="user_stend">
+//                4          
+//          </div>
+     
+//      </div>            
+//  </div>
+//      `
+//    }
+//   return `<head>
+//             <h1 class="slide_title">${data.title}</h1>
+//             <h2 class="slide_subtitle">${data.subtitle}</h2>
+//           </head>
             
-  `
+            
+//   `
 }
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-     // Глобальная функция рендера шаблона
-    
     //Получение параметров из адресной строки
   var getQueryParam = function getQueryParam (param) {
     var queries = window.location.search, regex, resRegex, results, response;
